83a22cef0bd93f1741dc1a8bc1980a0cac87dee5
Vitalic6661 <62883372+Vitalic6661@users.noreply.github.com>
2020-04-04 14:37:14 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+      caches.open("static")
+        .then(function(cache) {
+          console.log("precaching");
+          cache.add('/index.html');
+          cache.add('/css/bootstrap.css');
+          cache.add('/images/logo.png');
+          cache.add('/');
+        })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+console.log('[Service Worker] Activating Service Worker ...', event);
+return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if(response)
+          return response;
+        else
+          return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('push', event => {
+  const notification = event.data.text();
+  self.registration.showNotification(notification, {});
+});
